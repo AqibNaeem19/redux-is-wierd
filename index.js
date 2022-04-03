2715ddd21f92923efd3680d1a5ec69635262a8df
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-04-03 13:00:29 +0500

    created reducer function

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ function buyCake(){
     info: 'First redux action'
   }
 }
+
+const initialState = {
+  numOfCakes: 100
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type){
+    case BUY_CAKE: return {
+      // to make a copy of the state first, then updates
+      ...state, 
+      numOfCakes: state.numOfCakes - 1
+    }
+    default: return state
+  }
+}
